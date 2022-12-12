efd3604233e50abe08b6d0be750c02c98fc69576
lannguyen <nguyentruongngoclan@gmail.com>
2022-12-11 18:35:34 -0800

    comments on main.go

--- main.go
+++ main.go
@@ -17,26 +17,31 @@ func main() {
 	logger := log.New(os.Stdout, "product-api", log.LstdFlags)
 	productHandler := handlers.NewProduct(logger)
 
+	// Create a new serve mux and register the handlers
 	serveMux := mux.NewRouter()
-	// getRouter will serve only GET methods
+
+	// Handlers for APIs
 	getRouter := serveMux.Methods(http.MethodGet).Subrouter()
 	getRouter.HandleFunc("/", productHandler.GetProducts)
-	// putRouter will serve only PUT methods
+
 	putRouter := serveMux.Methods(http.MethodPut).Subrouter()
 	putRouter.HandleFunc("/{id:[0-9]+}", productHandler.UpdateProducts)
 	putRouter.Use(productHandler.MiddlewareValidateProduct)
-	// postRouter will serve only POST methods
+
 	postRouter := serveMux.Methods(http.MethodPost).Subrouter()
 	postRouter.HandleFunc("/", productHandler.AddProduct)
 	postRouter.Use(productHandler.MiddlewareValidateProduct)
-	// deleteRouter will serve only DELETE methods
+
 	deleteRouter := serveMux.Methods(http.MethodDelete).Subrouter()
 	deleteRouter.HandleFunc("/{id:[0-9]+", productHandler.DeleteProduct)
 
+	// Handler for documentation api /docs
 	opts := middleware.RedocOpts{SpecURL: "/swagger.yaml"}
 	docHandler := middleware.Redoc(opts, nil)
 	getRouter.Handle("/docs", docHandler)
 	getRouter.Handle("/swagger.yaml", http.FileServer(http.Dir("./")))
+
+	// Creates a new server
 	server := &http.Server{
 		Addr:         ":9090",
 		Handler:      serveMux,
@@ -44,19 +49,25 @@ func main() {
 		ReadTimeout:  1 * time.Second,
 		WriteTimeout: 1 * time.Second,
 	}
+
+	// Start the server
 	go func() {
 		err := server.ListenAndServe()
 		if err != nil {
 			logger.Fatal(err)
 		}
 	}()
+
+	// trap sigterm or interupt and gracefully shutdown the server
 	signalChannel := make(chan os.Signal)
 	signal.Notify(signalChannel, os.Interrupt)
 	signal.Notify(signalChannel, os.Kill)
 
+	// Block until a signal is received.
 	sig := <-signalChannel
 	logger.Println("Received terminate, graceful shutdown", sig)
 
+	// gracefully shutdown the server, waiting max 30 seconds for current operations to complete
 	timeoutContext, _ := context.WithTimeout(context.Background(), 30*time.Second)
 
 	server.Shutdown(timeoutContext)
